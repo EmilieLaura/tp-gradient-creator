e5c96ef021f737385af4f407e1d9775a4b7a838c
EmilieLaura <eaccipe@gmail.com>
2022-03-18 14:17:50 +0100

    Ajout Gradien

--- src/Components/GradientGenerator.js
+++ src/Components/GradientGenerator.js
@@ -3,12 +3,12 @@ import Color from "./Color";
 
 export default function GradientGenerator() {
 
+    const color = {background: "linear-gradient(90deg, #f1f1f1 20%, #323D8B 80%"};
+
     return (
         <div>
-            <div className="col-10 mx-auto block-result gradient-block">
-                
-            </div>
-            <p>Gradient values :</p>
+            <div className="col-10 mx-auto block-result gradient-block"  style={color}></div>
+            <p>Gradient values : </p>
         </div>
 
     )
